ef18a1c24e132f13fc74b55cebd01730efdaf8ee
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2023-05-24 16:09:17 -0300

    OOP approach

--- model/ProductDAO.php
+++ model/ProductDAO.php
@@ -1,5 +1,6 @@
 <?php
     namespace scandiweb\model;
+        
     use PDO;
     interface DAO
     {
@@ -7,7 +8,7 @@
         public function delete(int $id);
     }
 
-    final class ProductDAO implements DAO
+    class ProductDAO implements DAO
     {
         protected object $conn;
 
@@ -15,23 +16,7 @@
         {
             $this->conn = $conn;
         }
-
-        public function save(Product $p)
-        {
-            $sql = 'insert into products(sku, name, price, size, height, width, length, weight, product_type) values(:sku, :name, :price, :size, :height, :width, :length, :weight, :type)';
-            $insert = $this->conn->prepare($sql);
-            $insert->bindValue(':sku', $p->getSKU());
-            $insert->bindValue(':name', $p->getName());
-            $insert->bindValue(':price', $p->getPrice());
-            $insert->bindValue(':size', $p->getSize());
-            $insert->bindValue(':height', $p->getHeight());
-            $insert->bindValue(':width', $p->getWidth());
-            $insert->bindValue(':length', $p->getLength());
-            $insert->bindValue(':weight', $p->getWeight());
-            $insert->bindValue(':type', $p->getType());
-            $insert->execute();
-        }
-
+        
         public function getAllProducts()
         {
             $sql = 'select * from products';
@@ -50,5 +35,53 @@
             $select->bindValue(':id', $id);
             $select->execute();
         }
-    };
+    }
+
+    final class DVDDAO extends ProductDAO 
+    {
+        public function save(DVD $p)
+        {
+            $sql = 'insert into products(sku, name, price, size, product_type) values(:sku, :name, :price, :size, :type)';
+            $insert = $this->conn->prepare($sql);
+            $insert->bindValue(':sku', $p->getSKU());
+            $insert->bindValue(':name', $p->getName());
+            $insert->bindValue(':price', $p->getPrice());
+            $insert->bindValue(':size', $p->getSize());
+            $insert->bindValue(':type', $p->getType());
+            $insert->execute();
+        }
+    }
+
+    final class BookDAO extends ProductDAO 
+    {
+        public function save(Book $p)
+        {
+            $sql = 'insert into products(sku, name, price, weight, product_type) values(:sku, :name, :price, :weight, :type)';
+            $insert = $this->conn->prepare($sql);
+            $insert->bindValue(':sku', $p->getSKU());
+            $insert->bindValue(':name', $p->getName());
+            $insert->bindValue(':price', $p->getPrice());
+            $insert->bindValue(':weight', $p->getWeight());
+            $insert->bindValue(':type', $p->getType());
+            $insert->execute();
+        }
+    }
+
+    final class FurnitureDAO extends ProductDAO 
+    {
+        public function save(Furniture $p)
+        {
+            $sql = 'insert into products(sku, name, price, height, width, length, product_type) values(:sku, :name, :price, :height, :width, :length, :type)';
+            $insert = $this->conn->prepare($sql);
+            $insert->bindValue(':sku', $p->getSKU());
+            $insert->bindValue(':name', $p->getName());
+            $insert->bindValue(':price', $p->getPrice());
+            $insert->bindValue(':height', $p->getHeight());
+            $insert->bindValue(':length', $p->getLength());
+            $insert->bindValue(':width', $p->getWidth());
+            $insert->bindValue(':type', $p->getType());
+            $insert->execute();
+        }
+    }
+    
 ?>
